7d175c3d100ffac91d25192b94465eea34800541
Christian <bournecreative@gmail.com>
2025-08-15 00:18:55 -0500

    starter ts project configured with eslint and prettier

---
+++ vite-ts-eslint-prettier/eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended, // integrates Prettier rules with eslint
+  // Add any custom rules or plugins here
+];
