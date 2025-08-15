14e4cab306a7a365bd236b4a5a13d3c2cf86829d
Christian <bournecreative@gmail.com>
2025-08-15 00:30:51 -0500

    typescript build with jest, eslint and prettier configured

---
+++ vite-ts-jest-eslint-prettier/src/counter.test.ts
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+
+test('Increment button is appended to the document', () => {
+  document.body.innerHTML = `<button id="counter" type="button"></button>`;
+  const btnEle = document.getElementById('counter');
+  expect(btnEle).toBeInTheDocument();
+});
